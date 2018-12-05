28cb0b155a8cbff483951264bcc7f7741de3bc18
ninjacraft123 <42971687+ninjacraft123@users.noreply.github.com>
2018-12-05 17:36:08 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,142 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'هنا') {
+    msg.reply('ورع اعملي بوت @ZackGabri#7771');
+  }
+});
+
+const prefix = 'L'
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Lhelp `,"https://www.twitch.tv/dggamingbot")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+client.on('message', message => { // هاذا للبرودكسات
+        var prefix = 'L'; // هنا تقدر تغير البرفكس
+	var command = message.content.split(" ")[0];
+	if(command == prefix + 'bc') { // الكوماند !bc
+		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don`t have **MANAGE_MESSAGES** permission!");
+		var args = message.content.split(' ').slice(1).join(' ');
+		if(message.author.bot) return;
+		if(!args) return message.channel.send(`**➥ Useage:** ${prefix}bc كلامك`);
+		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don`t have **MANAGE_MESSAGES** permission!");
+		
+		let bcSure = new Discord.RichEmbed()
+		.setTitle(`:mailbox_with_mail: **هل انت متأكد انك تريد ارسال رسالتك الى** ${message.guild.memberCount} **عضو**`)
+		.setThumbnail(client.user.avatarURL)
+		.setColor('RANDOM')
+		.setDescription(`**\n:envelope: ➥ رسالتك**\n\n${args}`)
+		.setTimestamp()
+		.setFooter(message.author.tag, message.author.avatarURL)
+		
+		message.channel.send(bcSure).then(msg => {
+			msg.react('✅').then(() => msg.react('❎'));
+			message.delete();
+			
+			
+			let yesEmoji = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
+			let noEmoji = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
+			
+			let sendBC = msg.createReactionCollector(yesEmoji);
+			let dontSendBC = msg.createReactionCollector(noEmoji);
+			
+			sendBC.on('collect', r => {
+				        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#000000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+				message.channel.send(`:timer: **يتم الان الارسال الى** \`\`${message.guild.memberCount}\`\` **عضو**`).then(msg => msg.delete(5000));
+				msg.delete();
+			})
+			dontSendBC.on('collect', r => {
+				msg.delete();
+				message.reply(':white_check_mark: **تم الغاء ارسال رسالتك بنجاح**').then(msg => msg.delete(5000));
+			});
+		})
+	}
+});
+	
+	client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي', `Welcome to the server ${member}`)
+        .addField('User ID :', `"[" + ${member.id} + "]"` )
+                .addField(':arrow_down:',`** انت العضو رقم ${member.guild.memberCount}**`)
+
+                  .addField("Name:",`**<@${member.id}>**`, true)
+
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+
+     .setFooter('Server To Legends')
+        .setTimestamp()
+
+      channel.sendEmbed(embed);
+});
+
+    client.on('message', message => { // Falsteni Is Here
+    const responses = [
+    'yes', 'no', 'خليك في حالك', 'شفلك كلبه', 'sure', 'ask me later', 'Fuck you','شكرا'
+    ]
+        if(message.content.startsWith(prefix + '-')) { // Falsteni Is Here
+                if(!message.channel.guild) return;
+            let args = message.content.split(" ").slice(1); // Falsteni Is Heressss
+            if (!args[0]) {
+                message.channel.send('**Please type Your question!**');
+                return;
+                }
+       message.channel.sendMessage(`${responses[Math.floor(Math.random() * responses.length)]}`); // Falsteni Is Here
+                   if (!args[0]) {
+              message.edit('1')
+              return; // Falsteni Is Here
+            } // Falsteni Is Here
+        } // Falsteni Is Here
+    }); // Falsteni Is Here
+	
+
+ client.on('message', message => { // هيلب كوماند
+var prefix = 'L'
+    if (message.author.bot) return;
+     if (message.content === prefix + "roles") {
+         message.channel.send(`**Roles Count = ${message.guild.roles.size}**`);
+     }
+});
+
+client.login(process.env.BOT_TOKEN);
